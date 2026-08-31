cafea4be92af634a9736746e76bc57cfe7c19531
Guilherme Brito Dos Santos <guidobritosantosss@gmail.com>
2026-08-31 16:51:41 -0300

    Feat: Seção hero
    Adição da seção hero com imagem e organização de layout

--- gui-dev-portfolio/src/components/Button.jsx
+++ gui-dev-portfolio/src/components/Button.jsx
@@ -1,5 +1,5 @@
 export const Button = ({ className = "", tamanho="default", texto }) => {
-    const classesBase = "relative overflow-hidden rounded-full font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-primary bg-primary text-primary-foreground hover:bg-primary/90 shadow-lg shadow-primary/25"
+    const classesBase = "relative overflow-hidden rounded-full font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-primary bg-primary text-primary-foreground hover:bg-primary/90 shadow-lg shadow-primary/50"
 
     const classesTamanho = {
         sm: "px-4 py-2 text-sm",
--- gui-dev-portfolio/src/sections/Hero.jsx
+++ gui-dev-portfolio/src/sections/Hero.jsx
@@ -1,5 +1,15 @@
+import HeroBg from "../assets/hero/hero-bg.png"
+
 export const Hero = () => {
     return (
-        <header></header>
+        <section>
+            <figure className="absolute inset-0 -z-1">
+                <img src={HeroBg} alt="Foto do espaço sideral com dois planetas" className="w-full h-full object-cover"/>
+            </figure>
+
+            <div className="absolute inset-0 bg-gradient-to-b from-background/40 via-background/60 to-background -z-1">
+
+            </div>
+        </section>
     )
 } 
